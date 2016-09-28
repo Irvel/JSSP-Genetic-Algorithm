8a8947a8b9f2e33675b7ee03dfba39f42bae0175
Jorge Gil <jorgegilcavazos@gmail.com>
2016-09-28 14:00:41 -0500

    Add data matrix

--- main.py
+++ main.py
@@ -7,5 +7,23 @@ def is_permutation_valid(permutation):
 def get_random_permutation():
 	"""return a random permutation from the pool"""
 
+num_operations = 10
 
+""" 
+operations is a Nx4 matrix representing each operation
+and its Job, Order, Machine, and operation Time.
+E.g.
+
+	 J  O  M   T
+Op1 [1, 1, 2, 10]
+Op2 [2, 1, 1,  5]
+Op3 [1, 2, 1, 20]
+Op4 [1, 3, 3, 50]
+Op5 [2, 2, 2,  3]
+"""
+operations[]
+for i in range(num_operations):
+	operations.append([])
+	for j in range(4):
+		operations[i].append(None)
 

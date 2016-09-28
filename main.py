874378748340d77ee139e61dd126ca53b252c0f3
Jorge Gil <jorgegilcavazos@gmail.com>
2016-09-28 13:32:32 -0500

    Add main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+def permute(n):
+	"""return a list of n valid permutations"""
+
+def is_permutation_valid(permutation):
+	return False
+
+def get_random_permutation():
+	"""return a random permutation from the pool"""
+
+
+

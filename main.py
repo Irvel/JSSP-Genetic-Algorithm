c8fc3c99b147b296681bec71e3ff741c2d6cfd61
EdgarB <edgar_daniel_1996@hotmail.com>
2016-09-28 15:44:19 -0500

    calculate_makespan function added

--- main.py
+++ main.py
@@ -7,4 +7,24 @@ def is_permutation_valid(permutation):
 def get_random_permutation():
 	"""return a random permutation from the pool"""
 
-def calculate_makespan(permutation)
+def calculate_makespan(permutation):
+	cummulative_machine_times = {}
+	cummulative_job_times = {}
+
+	for operation in permutation:
+		#initialize variables with 0 if does not exist
+		if !operation.job in cummulative_job_times:
+			cummulative_job_times[operation.job] = 0
+
+		if !operation.machine in cummulative_machine_times:
+			cummulative_machine_times[operation.machine] = 0
+			
+		if cummulative_job_times[operation.job] < cummulative_machine_times[operation.machine]:
+			cummulative_machine_times[operation.machine] += operation.duration
+		 	cummulative_job_times[operation.job] = cummulative_machine_times[operation.machine]
+		else:
+			cummulative_job_times[operation.job] += operation.duration
+			cummulative_machine_times[operation.machine] = cummulative_job_times[operation.job]
+
+	#Return the biggest time of the jobs
+	return cummulative_job_times[max(cummulative_job_times, key=cummulative_job_times.get)]

26f6de9b25ca4856a3673c30beafbccb0144f7ee
Irvel <irvel.nduva@gmail.com>
2016-09-28 14:31:16 -0500

    Add description to base classes

--- genome.py
+++ genome.py
@@ -1,4 +1,10 @@
-class Operation:
+"""
+genome.py
+~~~~~~~~~~~~~
+The class Genome stores the set of operations that compose an individual in the
+population.
+"""
+class Genome:
     def __init__(self, operations):
         self.operations = operations
         self.score = 0

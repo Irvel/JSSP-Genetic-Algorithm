b58e855ea39801afdaf6f9834b79674235b9f267
Irvel <irvel.nduva@gmail.com>
2016-09-28 14:30:52 -0500

    Add fitness function

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+BAD_SCORE = -10000
+
 def permute(n):
 	"""return a list of n valid permutations"""
 
-def is_permutation_valid(permutation):
+def is_valid_permutation(permutation):
 	return False
 
 def get_random_permutation():
 	"""return a random permutation from the pool"""
 
 def calculate_makespan(permutation)
+
+def calculate_fitness(permutation):
+    penalization = 0
+    if !is_valid_permutation(permutation):
+        penalization = BAD_SCORE
+    make_span = calculate_makespan(permutation)
+    score = make_span + penalization
+    return score

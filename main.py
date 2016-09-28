99582229673ffb6665ef2414dfb94da88aabdd97
Jorge Gil <jorgegilcavazos@gmail.com>
2016-09-28 15:03:56 -0500

    Add random matrix generator

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import random
+
+def print_matrix(matrix, rows, cols):
+	for i in range(rows):
+		for j in range(cols):
+			print (matrix[i][j], end=" ")
+		print ("")
+
+def fill_matrix(matrix, num_operations, num_columns, operations_per_job, num_machines):
+	job_number = 1
+	job_count = 1
+	for i in range(num_operations):
+		if (job_count > operations_per_job):
+			job_count = 1
+			job_number += 1
+		matrix[i][0] = job_number
+		matrix[i][1] = job_count
+		matrix[i][2] = random.randrange(1, num_machines + 1, 1)
+		matrix[i][3] = random.randrange(5, 100, 5)
+		job_count += 1
+
 def permute(n):
 	"""return a list of n valid permutations"""
 
@@ -7,10 +28,13 @@ def is_permutation_valid(permutation):
 def get_random_permutation():
 	"""return a random permutation from the pool"""
 
-def calculate_makespan(permutation)
+def calculate_makespan(permutation):
+	""""""
 
-num_operations = 10
-
+num_operations = 11
+num_columns = 4
+num_machines = 3
+max_operations_per_job = 3
 """ 
 operations is a Nx4 matrix representing each operation
 and its Job, Order, Machine, and operation Time.
@@ -23,8 +47,12 @@ Op3 [1, 2, 1, 20]
 Op4 [1, 3, 3, 50]
 Op5 [2, 2, 2,  3]
 """
-operations[]
+operations = []
 for i in range(num_operations):
 	operations.append([])
-	for j in range(4):
+	for j in range(num_columns):
 		operations[i].append(None)
+
+fill_matrix(operations, num_operations, num_columns, max_operations_per_job, num_machines)
+print_matrix(operations, num_operations, num_columns)
+

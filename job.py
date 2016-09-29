72b4a0da26abaee0aa1087083a02a165d04f8f08
Gerardo Galvez <gerardogv96@hotmail.com>
2016-09-29 15:33:10 -0500

    Added job_ids

--- job.py
+++ job.py
@@ -1,23 +1,23 @@
 from operation import Operation
 
 class Job:
-	def __init__(self, start_date, goal_date, model):
+	def __init__(self, start_date, goal_date, model, job_id):
 		self.start_date = start_date
 		self.goal_date  = goal_date
 		self.model = model
 		self.operations = []
 
 		if model == "1111":
-			o1 = Operation("m1", 3, "1111")
-			o2 = Operation("m2", 4, "1111")
-			o3 = Operation("m1", 2, "1111")
+			o1 = Operation("m1", 3, "1111", job_id)
+			o2 = Operation("m2", 4, "1111", job_id)
+			o3 = Operation("m1", 2, "1111", job_id)
 
 			self.operations.extend([o1, o2, o3])
 			o3.dependencies = [o1, o2]
 		elif model == "2222":
-			o1 = Operation("m2", 4, "2222")
-			o2 = Operation("m1", 2, "2222")
-			o3 = Operation("m2", 3, "2222")
+			o1 = Operation("m2", 4, "2222", job_id)
+			o2 = Operation("m1", 2, "2222", job_id)
+			o3 = Operation("m2", 3, "2222", job_id)
 
 			self.operations.extend([o1, o2, o3])
 			o3.dependencies = [o1, o2]

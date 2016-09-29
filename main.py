028016efe6941c41799f9c7d12e78d392efa5001
EdgarB <edgar_daniel_1996@hotmail.com>
2016-09-29 15:36:45 -0500

    makespan function updated

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from population import Population
 from job import Job
 import random
 from operation import Operation
-from population import is_valid_permutation
+from population import is_valid_permutation, calculate_makespan
 from datetime import datetime, timedelta
 
 def print_matrix(matrix, rows, cols):
-    for i in range(rows):
-        for j in range(cols):
-            print (matrix[i][j], end=" ")
-        print ("")
+	for i in range(rows):
+		for j in range(cols):
+			print (matrix[i][j], end=" ")
+		print ("")
 
 def fill_matrix(matrix, num_operations, num_columns, operations_per_job, num_machines):
 	job_number = 1
@@ -25,20 +25,20 @@ def fill_matrix(matrix, num_operations, num_columns, operations_per_job, num_mac
 		job_count += 1
 
 def get_operations_list(matrix, num_operations):
-    operations_list = []
-    for i in range(num_operations):
-        job_num = matrix[i][0]
-        order_num = matrix[i][1]
-        machine_num = matrix[i][2]
-        time = matrix[i][3]
-        operations_list.append(Operation(machine_num, time, job_num, order_num))
+	operations_list = []
+	for i in range(num_operations):
+		job_num = matrix[i][0]
+		order_num = matrix[i][1]
+		machine_num = matrix[i][2]
+		time = matrix[i][3]
+		operations_list.append(Operation(machine_num, time, job_num, order_num))
 
-    return operations_list
+	return operations_list
 
 if __name__ == "__main__":
 	products = {}
 	products['1111'] = 2
-	products['2222'] = 3
+	products['2222'] = 1
 
 	all_jobs = []
 	all_operations = []
@@ -51,6 +51,6 @@ if __name__ == "__main__":
 	for operation in all_operations:
 		print(operation)
 
-
-
-    
+	var1, var2 = calculate_makespan(all_operations)
+	print(str(var2))
+	print(var1)
--- population.py
+++ population.py
@@ -13,103 +13,103 @@ SIZE = 100
 BAD_SCORE = 1000
 
 class Population:
-    def __init__(self, operations):
-        self.genomes = []
-        self.create_new_population(operations)
-        self.sort_population()
-        self.reap_population()
+	def __init__(self, operations):
+		self.genomes = []
+		self.create_new_population(operations)
+		self.sort_population()
+		self.reap_population()
 
-    def __str__(self):
-        genomes_string = ""
-        for genome in self.genomes:
-            genomes_string += str(genome)
-        return genomes_string
+	def __str__(self):
+		genomes_string = ""
+		for genome in self.genomes:
+			genomes_string += str(genome)
+		return genomes_string
 
-    def create_new_population(self, operations):
-        permutations = product(operations, repeat=len(operations))
-        for _ in range(SIZE * 1000):
-            random.shuffle(operations)
-            genome = Genome(operations[:])
-            genome.score = calculate_fitness(genome.operations)
-            self.genomes.append(genome)
+	def create_new_population(self, operations):
+		permutations = product(operations, repeat=len(operations))
+		for _ in range(SIZE * 1000):
+			random.shuffle(operations)
+			genome = Genome(operations[:])
+			genome.score = calculate_fitness(genome.operations)
+			self.genomes.append(genome)
 
-    def sort_population(self):
-        """
-        Sorts the population based on the fitness score
-        """
-        self.genomes.sort(key = attrgetter('score'), reverse = False)
+	def sort_population(self):
+		"""
+		Sorts the population based on the fitness score
+		"""
+		self.genomes.sort(key = attrgetter('score'), reverse = False)
 
-    def reap_population(self):
-        """
-        Keeps only the first SIZE individuals
-        """
-        self.genomes = self.genomes[:100]
+	def reap_population(self):
+		"""
+		Keeps only the first SIZE individuals
+		"""
+		self.genomes = self.genomes[:100]
 
-    def reproduce_population(self):
-        """
-        The miracle of life
-        """
-        pass
+	def reproduce_population(self):
+		"""
+		The miracle of life
+		"""
+		pass
 
 def calculate_fitness(permutation):
-    penalization = 0
-    if not is_valid_permutation(permutation):
-        penalization = BAD_SCORE
-    make_span = calculate_makespan(permutation)
-    score = make_span + penalization
-    return score
+	penalization = 0
+	if not is_valid_permutation(permutation):
+		penalization = BAD_SCORE
+	make_span = calculate_makespan(permutation)
+	score = make_span + penalization
+	return score
 
 
 def is_valid_permutation(permutation):
-    operation_done = {}
-    for op in permutation:
-        operation_done[op] = 0
+	operation_done = {}
+	for op in permutation:
+		operation_done[op] = 0
 
-    for op in permutation:
-        for dep in op.dependencies:
-            if operation_done[dep] == 0:
-                return False
-        operation_done[op] = 1  
-    return True
+	for op in permutation:
+		for dep in op.dependencies:
+			if operation_done[dep] == 0:
+				return False
+		operation_done[op] = 1
+	return True
 
 def calculate_makespan(permutation):
-    cummulative_machine_times = {}
-    operations_end_time = {}
-    jobs_end_time = {}
+	cummulative_machine_times = {}
+	operations_end_time = {}
+	jobs_end_time = {}
 
-    for operation in permutation:
-        #initialize variables with 0 if does not exist
-        if not operation in operations_end_time:
-            operations_end_time[operation] = 0
+	for operation in permutation:
+		#initialize variables with 0 if does not exist
+		if not operation in operations_end_time:
+			operations_end_time[operation] = 0
 
-        if not operation.machine in cummulative_machine_times:
-            cummulative_machine_times[operation.machine] = 0
+		if not operation.machine in cummulative_machine_times:
+			cummulative_machine_times[operation.machine] = 0
 
-        #Check if the operation has dependencies
-        if operation.job.dependencies:
+		#Check if the operation has dependencies
+		if operation.dependencies:
 
-            #initialize time of the operation to the max value of dependencies
-            max_time_dependencies = operations_end_time[operation.dependencies[0]]
-            for dependent_operation in operation.dependencies:
-                if operations_end_time[dependent_operation] > max_time_dependencies:
-                    max_time_dependencies = operations_end_time[dependent_operation]
-            operations_end_times[operation] = max_time_dependencies
-
-
-        #Calculate time
-        if time_operation < cummulative_machine_times[operation.machine]:
-            cummulative_machine_times[operation.machine] += operation.duration
-            operations_end_time[operation] = cummulative_machine_times[operation.machine]
-        else:
-            operations_end_time[operation.job] += operation.duration
-            cummulative_machine_times[operation.machine] = operations_end_time[operation]
-
-        """Save the time of each operation.
-         So, at the end you have the last one is the one saved """
-        jobs_end_time[operation.job] = operations_end_time[operation]
+			#initialize time of the operation to the max value of dependencies
+			max_time_dependencies = operations_end_time[operation.dependencies[0]]
+			for dependent_operation in operation.dependencies:
+				if operations_end_time[dependent_operation] > max_time_dependencies:
+					max_time_dependencies = operations_end_time[dependent_operation]
+			operations_end_times[operation] = max_time_dependencies
 
 
+		#Calculate time
+		if operations_end_time[operation] < cummulative_machine_times[operation.machine]:
+			cummulative_machine_times[operation.machine] += operation.duration
+			operations_end_time[operation] = cummulative_machine_times[operation.machine]
+		else:
+			operations_end_time[operation] += operation.duration
+			cummulative_machine_times[operation.machine] = operations_end_time[operation]
 
-    """Return a map mapping each job with the corresponding end time,
-     and the biggest time of the jobs"""
-    return jobs_end_time, jobs_end_time[max(jobs_end_time, key=jobs_end_time.get)]
+		"""Save the time of each operation.
+		 So, at the end you have the last one is the one saved """
+		jobs_end_time[operation.job_id] = operations_end_time[operation]
+
+
+
+	"""Return a map mapping each job with the corresponding end time,
+	 and the biggest time of the jobs"""
+	return jobs_end_time, jobs_end_time[max(jobs_end_time, key=jobs_end_time.get)]

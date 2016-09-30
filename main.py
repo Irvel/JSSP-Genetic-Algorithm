47a45e98a8528381c3d56d945e91bfc32d95330e
Irvel <irvel.nduva@gmail.com>
2016-09-30 12:13:10 -0500

    Fix initial order of jobs to be the same everytime is ran

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from population import Population
 from job import Job
 import random
+import collections
 from operation import Operation
 from population import is_valid_permutation, calculate_makespan
 from datetime import datetime, timedelta
@@ -12,7 +13,7 @@ if __name__ == "__main__":
     root = tkinter.Tk()
     root.withdraw()
 
-    products = {}
+    products = collections.OrderedDict()
     products['5967'] = tkinter.simpledialog.askinteger("5967", "Quantity of model 5967?", minvalue=0, initialvalue=1)
     products['8047'] = tkinter.simpledialog.askinteger("8047", "Quantity of model 8047?", minvalue=0, initialvalue=1)
     products['4025'] = tkinter.simpledialog.askinteger("4025", "Quantity of model 4025?", minvalue=0, initialvalue=1)
@@ -33,12 +34,12 @@ if __name__ == "__main__":
     print(is_valid_permutation(all_operations))
     var1, var2 = calculate_makespan(all_operations)
     print(str(var2))
-    
+
     """
     for operation in all_operations:
         print(operation)
     """
-    
+
     population = Population(all_operations)
     print(population)
     current_best = population.genomes[1]
@@ -64,7 +65,7 @@ if __name__ == "__main__":
     for operation in current_best.operations:
         print(str(operation))
     print("Best makespan found: " + str(best_makespan))
-    
+
     plt.plot(generations, makespans, 'ro')
     plt.title("Initial population: " + str(100) + " Generations: " + str(40000))
     plt.ylabel("Makespan")

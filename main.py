f94deaac69084e3f568052ae575a21fcce36b16c
Gerardo Galvez <gerardogv96@hotmail.com>
2016-09-30 11:37:25 -0500

    Add plot makespan vs generation

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from operation import Operation
 from population import is_valid_permutation, calculate_makespan
 from datetime import datetime, timedelta
 import tkinter.simpledialog
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
@@ -41,9 +42,14 @@ if __name__ == "__main__":
     population = Population(all_operations)
     print(population)
     current_best = population.genomes[1]
-    print("\nReproducing population 4000 times...\n")
-    for i in range(4000):
+    makespans = []
+    generations = []
+    print("\nReproducing population 40000 times...\n")
+    for i in range(40000):
         if current_best is not population.genomes[0]:
+            current_makespan = calculate_makespan(current_best.operations)[1]
+            generations.append(i)
+            makespans.append(current_makespan)
             current_best = population.genomes[0]
             print("It #" + str(i) + ". The current best is: " +  str(current_best), end="")
             print("Improvement percentage: " + str(100 - calculate_makespan(current_best.operations)[1]/calculate_makespan(all_operations)[1] * 100)[:6] + "%")
@@ -53,10 +59,14 @@ if __name__ == "__main__":
     print(population)
     print("\n\n")
 
-
     dummy, best_makespan = calculate_makespan(current_best.operations)
     print("Best configuration found:\n")
     for operation in current_best.operations:
         print(str(operation))
     print("Best makespan found: " + str(best_makespan))
     
+    plt.plot(generations, makespans, 'ro')
+    plt.title("Initial population: " + str(100) + " Generations: " + str(40000))
+    plt.ylabel("Makespan")
+    plt.xlabel("Generation")
+    plt.show()
